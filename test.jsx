5da35ea9c572e43e3666788d413bd3e8a11f3ed9
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-20 19:15:14 +0545

    Initial

--- test.jsx
+++ test.jsx
@@ -1,6 +1,7 @@
 // Have a two Number INput Field and a button. On click of the button, add the two numbers and display the sum in a div.
-// Use Redux to manage the state of the application.
-// Use Functional Component with Hooks to build the application.
-// Use React-Redux to connect the store to the component.
-// Use Redux-Thunk to dispatch the action asynchronously.
-// Use Redux-Devtools-Extension to debug the application.
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { ToastContainer } from 'react-toastify';
+
