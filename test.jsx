e2b0c98d3b65ad93b767836fa73795531ce4dfb5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-20 19:15:54 +0545

    Initial

--- test.jsx
+++ test.jsx
@@ -1,7 +1,5 @@
 // Have a two Number INput Field and a button. On click of the button, add the two numbers and display the sum in a div.
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
-import { ToastContainer } from 'react-toastify';
+import React, { Component } from 'react'
+require('./test.css')
 
