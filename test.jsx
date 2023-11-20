a051dde63ece7121459e894a1e9b7c4561541900
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-20 19:14:39 +0545

    Initial

---
+++ test.jsx
@@ -0,0 +1,6 @@
+// Have a two Number INput Field and a button. On click of the button, add the two numbers and display the sum in a div.
+// Use Redux to manage the state of the application.
+// Use Functional Component with Hooks to build the application.
+// Use React-Redux to connect the store to the component.
+// Use Redux-Thunk to dispatch the action asynchronously.
+// Use Redux-Devtools-Extension to debug the application.
