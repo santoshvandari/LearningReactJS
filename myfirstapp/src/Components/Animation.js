9625bd830e4d92a03bfa038c4c54382d61462ff2
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-20 22:51:30 +0545

    FinalBuildUploaded

---
+++ myfirstapp/src/Components/Animation.js
@@ -0,0 +1,10 @@
+useEffect(() => {
+    if (currentIndex < text.length) {
+      const timeout = setTimeout(() => {
+        setCurrentText(prevText => prevText + text[currentIndex]);
+        setCurrentIndex(prevIndex => prevIndex + 1);
+      }, delay);
+  
+      return () => clearTimeout(timeout);
+    }
+  }, [currentIndex, delay, text]);
--- myfirstapp/src/Components/mainbody.js
+++ myfirstapp/src/Components/mainbody.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import Typing from './typing'
 
 const mainbody = ({imageurl}) => {
   return (
     <div className='imageWrapper'>
       <img src={imageurl} alt="Profilemage" />
       <h1>Santosh Bhandari</h1>
-      <p>Web Developer</p>
+      <p><Typing text='Web Developer' delay={200} infinite={false}/></p>
     </div>
   )
 }
