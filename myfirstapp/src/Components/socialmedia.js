b39c03fc4ad33374134f2641c13597cf8994509e
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-20 22:15:20 +0545

    MyFirstReactApp

--- myfirstapp/src/Components/socialmedia.js
+++ myfirstapp/src/Components/socialmedia.js
@@ -2,9 +2,13 @@ import React from 'react'
 
 export const SocialMedia = (props)=>{
     return (
-        <div>
-            <a href='https://www.facebook.com/santoshvandari.me'>Facebook</a>
-            <a href='https://www.github.com/santoshvandari'>GitHub</a>
+        <div className='wrapper'>
+            <h3>Get in Touch</h3>
+        <div className='SocialMedia'>
+            <a href='https://www.github.com/santoshvandari'><i class="bi bi-github"></i></a>
+            <a href='https://www.facebook.com/santoshvandari.me'><i class="bi bi-facebook"></i></a>
+            <a href='https://www.instagram.com/santosh.vandari'><i class="bi bi-instagram"></i></a>
+        </div>
         </div>
     );
 }
