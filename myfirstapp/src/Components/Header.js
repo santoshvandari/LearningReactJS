c5561fe9738b23e58862a4f8df087ae6ee97aa5b
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-22 18:22:13 +0545

    initial

--- myfirstapp/src/Components/Header.js
+++ myfirstapp/src/Components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react'
+// import { Link } from 'react-router-dom'
+import './Header.css'
 
-export const Header = () => {
+const Header = () => {
   return (
     <nav>
       <u>
-        <li> <link to="#">Home</link> </li>
-        <li> <link to="#">Home</link> </li>
-        <li> <link to="#">Home</link> </li>
-        <li> <link to="#">Home</link> </li>
+        {/* <li> <Link to="#">Home</Link> </li>
+        <li> <Link to="#">Services</Link> </li>
+        <li> <Link to="#">About</Link> </li>
+        <li> <Link to="#">Contact </Link> </li> */}
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
       </u>
     </nav>
   )
 }
+
+export default Header;
+
 
