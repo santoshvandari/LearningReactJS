30cf8f108ea75bf463a6dd32b297c064934fadc5
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-22 09:55:18 +0545

    Initial

--- myfirstapp/src/Components/Header.js
+++ myfirstapp/src/Components/Header.js
@@ -3,7 +3,12 @@ import React from 'react'
 export const Header = () => {
   return (
     <nav>
-      
+      <u>
+        <li> <link to="#">Home</link> </li>
+        <li> <link to="#">Home</link> </li>
+        <li> <link to="#">Home</link> </li>
+        <li> <link to="#">Home</link> </li>
+      </u>
     </nav>
   )
 }
