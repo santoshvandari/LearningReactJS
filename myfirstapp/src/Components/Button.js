484244e1e31e60c92f25030929071885dbd2a560
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-23 18:17:37 +0545

    Initial

--- myfirstapp/src/Components/Button.js
+++ myfirstapp/src/Components/Button.js
@@ -1,9 +1,10 @@
 import { React, useState,useEffect } from "react"
+import './Button.css'
 
 const Button = () => {
   return (
-    <div>
-      
+    <div className="btn-wrapper">
+      <button>Count</button>
     </div>
   )
 }
