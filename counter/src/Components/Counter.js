24c987f9e95cd1ebb3af6ebbb3c3cd564db6ee2a
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-23 18:47:26 +0545

    Initial

--- counter/src/Components/Counter.js
+++ counter/src/Components/Counter.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import './Counter.css'
 
 const Counter = () => {
   return (
-    <div>
-      Hello
+    <div className='container'>
+      <p className="count">0</p>
+        <div className="btn-wrapper">
+            <button>+</button>
+            <button>-</button>
+        </div>
     </div>
   )
 }
