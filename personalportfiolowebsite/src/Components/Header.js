4503a65a49554c227500593453dd29803cff57ef
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-24 11:59:11 +0545

    Initial

--- personalportfiolowebsite/src/Components/Header.js
+++ personalportfiolowebsite/src/Components/Header.js
@@ -5,18 +5,18 @@ const Header = () => {
         <div>
             {/* <!-- ======= Mobile nav toggle button ======= --> */}
             {/* <!-- <button type="button" class="mobile-nav-toggle d-xl-none"><i class="bi bi-list mobile-nav-toggle"></i></button> --> */}
-            <i class="bi bi-list mobile-nav-toggle d-lg-none"></i>
+            <i className="bi bi-list mobile-nav-toggle d-lg-none"></i>
             {/* <!-- ======= Header ======= --> */}
-            <header id="header" class="d-flex flex-column justify-content-center">
+            <header id="header" className="d-flex flex-column justify-content-center">
 
-                <nav id="navbar" class="navbar nav-menu">
+                <nav id="navbar" className="navbar nav-menu">
                     <ul>
-                        <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
-                        <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
-                        <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
-                        <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
-                        <li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Services</span></a></li>
-                        <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+                        <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></a></li>
+                        <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></a></li>
+                        <li><a href="#resume" className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></a></li>
+                        <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Portfolio</span></a></li>
+                        <li><a href="#services" className="nav-link scrollto"><i className="bx bx-server"></i> <span>Services</span></a></li>
+                        <li><a href="#contact" className="nav-link scrollto"><i className="bx bx-envelope"></i> <span>Contact</span></a></li>
                     </ul>
                 </nav>
                 {/* <!-- .nav-menu --> */}
