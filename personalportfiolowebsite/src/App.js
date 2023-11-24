dce99e001a413722600ddcc18a4ef416c039a3db
Santosh Bhandari <63558580+santoshvandari@users.noreply.github.com>
2023-11-24 12:01:58 +0545

    Initial

--- personalportfiolowebsite/src/App.js
+++ personalportfiolowebsite/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Header from "./Components/Header"
+import HeroBg from './Components/HeroBg';
 function App() {
   return (
     <div className="App">
       <Header/>
+      <HeroBg/>
       </div>
   );
 }
