ee864df7ad38e3c1cd04275974560c184da34ab0
Santosh Bhandari <santoshvandari1@gmail.com>
2025-09-01 18:15:46 +0545

    feat: accordion Create

--- ReactApps/accordian/src/App.js
+++ ReactApps/accordian/src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import faqs from "./constraints/faqs";
+import Accordion from "./components/Accordian";
 
-function App() {
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
---
+++ ReactApps/accordian/src/components/Accordian.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import AccordionItem from "./AccordianItem";
+
+export default function Accordion({ data }) {
+    const [curOpen, setCurOpen] = useState(null);
+
+    return (
+        <div className="accordion">
+            {data.map((el, i) => (
+                <AccordionItem
+                    curOpen={curOpen}
+                    onOpen={setCurOpen}
+                    title={el.title}
+                    num={i}
+                    key={el.title}
+                >
+                    {el.text}
+                </AccordionItem>
+            ))}
+
+            <AccordionItem
+                curOpen={curOpen}
+                onOpen={setCurOpen}
+                title="React Developers"
+                num={22}
+                key="React Developers"
+            >
+                <p>Allows React developers to:</p>
+                <ul>
+                    <li>Break up UI into components</li>
+                    <li>Make components reusuable</li>
+                    <li>Place state efficiently</li>
+                </ul>
+            </AccordionItem>
+        </div>
+    );
+}
+
