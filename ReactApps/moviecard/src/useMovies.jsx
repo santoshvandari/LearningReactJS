a216143924947a4e9e0cb7709b1aa8f53f688f42
Santosh Bhandari <santoshvandari1@gmail.com>
2025-09-04 10:48:26 +0545

    feat: create useMoves section

--- ReactApps/moviecard/src/useMovies.jsx
+++ ReactApps/moviecard/src/useMovies.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const KEY = "f84fc31d";
+const KEY = "<KEY_HERE>";
 
 export function useMovies(query) {
     const [movies, setMovies] = useState([]);
