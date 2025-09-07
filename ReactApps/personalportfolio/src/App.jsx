658fa8c3e3ad99efd82a03c187c40dad54a768c9
Santosh Bhandari <santoshvandari1@gmail.com>
2025-09-07 16:23:43 +0545

    feat: footer create

--- ReactApps/personalportfolio/src/App.jsx
+++ ReactApps/personalportfolio/src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import Header from "./component/Header"
+import Main from "./component/Main"
+import Footer from "./component/Footer"
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ ReactApps/personalportfolio/src/component/Footer.jsx
@@ -0,0 +1,19 @@
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-content">
+                <div className="footer-text">
+                    <p>&copy; <span id="date"></span> <strong>Santosh Bhandari</strong> - Personal Portfolio. All Rights Reserved.</p>
+                    <p style={{ fontSize: '0.9rem', color: '#64ffda', marginTop: '0.5rem' }}>
+                        Full-Stack Engineer | Nepal
+                    </p>
+                </div>
+
+            </div>
+        </footer>
+
+    )
+}
+
+export default Footer
