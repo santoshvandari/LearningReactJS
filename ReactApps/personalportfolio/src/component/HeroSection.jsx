cc409108c03b7036d68f2977dc26b3ac221cfda5
Santosh Bhandari <santoshvandari1@gmail.com>
2025-09-08 09:09:27 +0545

    feat: modify hero sections

--- ReactApps/personalportfolio/src/component/HeroSection.jsx
+++ ReactApps/personalportfolio/src/component/HeroSection.jsx
@@ -1,3 +1,4 @@
+import FloatingIcons from "./FloatingIcons"
 import HeroDescription from "./HeroDescription"
 import ImageSection from "./ImageSection"
 import ScrollIndicator from "./ScrollIndicator"
@@ -5,23 +6,14 @@ import ScrollIndicator from "./ScrollIndicator"
 function HeroSection() {
     return (
         <section id="home">
-            <div className="floating-icons">
-                <div className="floating-icon" data-tech="python"><i className="bi bi-code-slash"></i></div>
-                <div className="floating-icon" data-tech="database"><i className="bi bi-database"></i></div>
-                <div className="floating-icon" data-tech="api"><i className="bi bi-braces"></i></div>
-                <div className="floating-icon" data-tech="cloud"><i className="bi bi-cloud"></i></div>
-                <div className="floating-icon" data-tech="git"><i className="bi bi-git"></i></div>
-                <div className="floating-icon" data-tech="terminal"><i className="bi bi-terminal"></i></div>
-            </div>
+            <FloatingIcons />
             <div className="container">
                 <div className="hero-content">
                     <div className="profile-section" data-aos="fade-up" data-aos-delay="300">
                         <ImageSection />
-
                         <HeroDescription />
                     </div>
                 </div>
-
                 <ScrollIndicator />
             </div>
         </section>
