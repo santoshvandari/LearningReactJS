8f8a1458506a500604a7311287d7ecb579a68617
Santosh Bhandari <santoshvandari1@gmail.com>
2025-09-28 09:30:52 +0545

    feat: some necesssary page create

---
+++ NextJS/movie-database/src/app/loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function loader() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default loader
---
+++ NextJS/movie-database/src/app/not-found.js
@@ -0,0 +1,14 @@
+import Button from '@/components/Button/Button'
+import React from 'react'
+
+function NotFound() {
+    return (
+            <div>
+                <h3>404 - Page Not Found</h3>
+                <p>The page you are looking for does not exist.</p>
+                <Button>Go To Home</Button>
+            </div>
+    )
+}
+
+export default NotFound
